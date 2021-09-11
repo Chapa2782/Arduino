0524dfafb18c53f387778b1e5b0f271c462e2e21
Chapa2782 <sealtomail@gmail.com>
2021-09-11 02:18:08 -0300

    Nueva actualizacion con OTA

--- Portonwifipulsador/PortonWifiPulsador.ino
+++ Portonwifipulsador/PortonWifiPulsador.ino
@@ -7,14 +7,17 @@
 #include <Adafruit_MQTT.h>
 #include <ArduinoHttpClient.h>
 
-#define IO_USERNAME    "Chapa2782"
-#define IO_KEY         "aio_uAPD14CCOYnGIu0WwhRJvswNrqSN"
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+#define IO_USERNAME  "Chapa2782"
+#define IO_KEY       "aio_bGeY18bBrsZui0vzwzgEq0AfKtz2"
 
 
 /******************************* WIFI Configuration **************************************/
 
-#define WIFI_SSID ""
-#define WIFI_PASS ""
+#define WIFI_SSID "CHAPARED"
+#define WIFI_PASS "Sealto8227"
 
 
 #include "AdafruitIO_WiFi.h"
@@ -26,9 +29,9 @@ AdafruitIO_Feed *command = io.feed("porton");
 
 int MOTORC = D0;  //D4
 
-String DISPVIS = "SealtoPorton_AAA002";
-String DISP = "/Dispositivos/AAA002/";
-int numeroDispo = 1;
+String DISPVIS = "SealtoPorton_AAA001";
+String DISP = "/Dispositivos/AAA001/";
+int numeroDispo = 0;
 int Estado = 0;
 
 
@@ -45,62 +48,114 @@ int Estado = 0;
 
 FirebaseData firebaseData;
 
+int cont = 0;
+
 void setup() {
   pinMode(MOTORC, OUTPUT);
-  digitalWrite(MOTORC,HIGH);
+  digitalWrite(MOTORC,LOW);
 
   Serial.begin(115200);
   WiFiManager wifiManager;
  
   
-  wifiManager.autoConnect("SealtoPorton_AAA002","123456");
+  wifiManager.autoConnect("NewSealtoPorton","123456");
   //wifiManager.resetSettings();
   
-  command->onMessage(handleMessage);
+  io.connect();
 
-  // wait for a connection
+   command->onMessage(handleMessage);
+  
   while(io.status() < AIO_CONNECTED) {
     Serial.print(".");
     delay(500);
   }
-  command-> get();
+  delay(2000);
+
+  command ->get();
 
   // we are connected
   Serial.println(io.statusText());
  
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
+  //while (WiFi.status() != WL_CONNECTED) {
+    //delay(500);
+    //Serial.print(".");
+  //}
   Serial.println(WiFi.localIP());
+
+   Serial.println("io estatus");
+  Serial.println(io.statusText());
    
   Firebase.begin("https://sealtoporton.firebaseio.com/", "GEASjeDTH6KLc0wAf7jLNjEAugGIMfKi2ZJWBTQ3");
   Firebase.reconnectWiFi(true);
 
-  
-  //firebaseData.setBSSLBufferSize(5,5);
+  Firebase.setMaxRetry(firebaseData, 3);
 
-  //firebaseData.setResponseSize(1024);
-
-  //6. Optional, set number of error retry
- Firebase.setMaxRetry(firebaseData, 0);
-
-//7. Optional, set number of error resumable queues
-Firebase.setMaxErrorQueue(firebaseData, 0);
+  Firebase.setMaxErrorQueue(firebaseData, 10);
 
   Firebase.setReadTimeout(firebaseData, 1000 * 60 * 15); // estaba en 60 * 1000
   
-  //Firebase.setwriteSizeLimit(firebaseData, "tiny");
   Firebase.setInt(firebaseData, DISP + "Comando",3);
   Firebase.setInt(firebaseData, DISP + "Estado",0);
+  Serial.println("Comienza inicio con OTA...");
+
+
+  //Comienza seccion OTA
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+   ArduinoOTA.setHostname("SealtoSoftPorton1");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
  
 void loop() {
- LecturaPulsador();
+  ArduinoOTA.handle();
  io.run();
+ LecturaPulsador();
 }
 void LecturaPulsador(){
-   WiFiManager wifiManager;
   if (Firebase.getInt(firebaseData, DISP + "Comando")) {
     if  (firebaseData.dataType() == "int") {
       int val = firebaseData.intData();
@@ -127,11 +182,6 @@ void LecturaPulsador(){
             Firebase.setInt(firebaseData, DISP + "DEV",val);
             
             break;
-        case 4:
-            Firebase.setInt(firebaseData, DISP + "DEV",val);
-            wifiManager.resetSettings();
-            ESP.reset();
-            break;
         }
       }
     }
@@ -142,8 +192,6 @@ void LecturaPulsador(){
         Estado = val;
       }
     }
-
-    
 }
 
 
@@ -151,12 +199,29 @@ void handleMessage(AdafruitIO_Data *data) {
   int command = data->toInt();
   Serial.print("Comando...");
   Serial.println(command + (numeroDispo * 3));
+  if(cont == 0){
+    Serial.println("Ingreso por primera vez");
+    cont  = 1;
+    return;
+  }
   if (command == cAbrir + (numeroDispo * 3)){ //light up the LED
-    Firebase.setInt(firebaseData, DISP + "Comando",1); 
-    //Pulsador();
+    //Firebase.setInt(firebaseData, DISP + "Comando",1); 
+     if(Estado == 0){
+        Firebase.setInt(firebaseData, DISP + "Estado",2);
+     }
+     if(Estado == 2){
+        Firebase.setInt(firebaseData, DISP + "Estado",0);
+     }
+    Pulsador();
   }else if(command == cCerrar + (numeroDispo * 3)){
-    Firebase.setInt(firebaseData, DISP + "Comando",2); 
-    //Pulsador();
+    //Firebase.setInt(firebaseData, DISP + "Comando",2); 
+    if(Estado == 0){
+        Firebase.setInt(firebaseData, DISP + "Estado",2);
+     }
+     if(Estado == 2){
+        Firebase.setInt(firebaseData, DISP + "Estado",0);
+     }
+    Pulsador();
   }else if(command == cParar + (numeroDispo * 3)){
     //Firebase.setInt(firebaseData, DISP + "Comando",0);
     //Pulsador();
@@ -164,9 +229,11 @@ void handleMessage(AdafruitIO_Data *data) {
 }
 
 void Pulsador(){
-  digitalWrite(MOTORC,LOW);
-  delay(2000);
   digitalWrite(MOTORC,HIGH);
+  delay(2000);
+  digitalWrite(MOTORC,LOW);
+  
+  //Serial.println("Ingreso en pulsador function");        
   Firebase.setInt(firebaseData, DISP + "Comando",3);
 }
 
